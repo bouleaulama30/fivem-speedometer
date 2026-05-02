eaf4fb95d1d719db920a61aa3cf7814dbbb04994
leo <leo.lefebvre@telecom-paris.fr>
2026-05-01 23:49:11 -0400

    base config

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'bouleaulama30'
+description 'Script speedometer for fiveM'
+version '1.0.0'
+
+
+client_scripts {
+    'client/client.lua',
+}
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/styles/main.css',
+    'ui/scripts/main.js',
+}
